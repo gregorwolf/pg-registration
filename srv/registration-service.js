46f56a1f2d51fc0e0363c1f760d1760b9ae70ae4
Gregor Wolf <gregor.wolf@gmail.com>
2021-11-25 18:59:08 +0100

    use let instead of const

--- srv/registration-service.js
+++ srv/registration-service.js
@@ -3,7 +3,7 @@ const { retrieveJwt, verifyJwt } = require("@sap-cloud-sdk/core");
 
 module.exports = async function (srv) {
   srv.on("READ", "UserScopes", async (req) => {
-    const user = {
+    let user = {
       username: req.user.id,
       is_admin: req.user.is("admin"),
     };
